4adfd7ac6249d336d47aa2202bc524c186552b92
Fernando <fernando@flashbob.de>
2022-12-06 22:53:22 +0100

    add cmc.py

---
+++ cmc.py
@@ -0,0 +1,26 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+from key import key
+import json
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+parameters = {
+  'id':'1',
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': key,
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url, params=parameters)
+  data = json.loads(response.text)
+  price = int(data["data"]["1"]["quote"]["USD"]["price"])
+  with open("btcprice.txt", "w") as f:
+    f.write(str(price))
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
